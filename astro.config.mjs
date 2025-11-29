5773b2392f52df9a652be7309590e849b3e989ba
Marek Dolnicek <marekdolnicek@gmail.com>
2025-11-30 00:53:32 +0100

    feat: update Astro configuration to use server output and add Node integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mdolnicek.eu',
   srcDir: './src',
-  output: 'static',
+  output: 'server',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'cs', 'de', 'es', 'it', 'fr'],
@@ -20,6 +21,9 @@ export default defineConfig({
     react(),
     tailwind({ applyBaseStyles: false }),
     mdx(),
-    sitemap()
+    sitemap(),
+    node({
+      mode: 'standalone'
+    })
   ]
 });
